505b42d7c2825d5bdcbeed0df9f166283d408c3e
Mahima <144243938+mahima-majumdar@users.noreply.github.com>
2024-09-16 11:28:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ let a = "5";
 const App = () => {
   return (
     <>
-      <div className='flex justify-center items-center bg-slate-800 h-[80vh] text-red-600  flex-col leading-none tracking-tight'>
+      <div className='flex justify-center rounded-xl items-center bg-slate-800 h-[80vh] text-red-600  flex-col leading-none tracking-tight'>
         <h1 className='text-7xl font-bold uppercase'>first React project</h1>
         <br />
         <h2 className='text-3xl text-red-400'> {import.meta.env.VITE_API_KEY}</h2>
